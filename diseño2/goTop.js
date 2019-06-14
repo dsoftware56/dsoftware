e448d76dac3fa1f1de213332544498e9e46e2a6f
dsoftware56 <dsoftware56@gamil.com>
2019-06-13 23:02:03 -0500

    ultimos cambios de la reunion en el depa

---
+++ diseño2/goTop.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){ //Hacia arriba
+  irArriba();
+});
+
+function irArriba(){
+  $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+}
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("topnav");
+var btns = header.getElementsByClassName("menuAct");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
