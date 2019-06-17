e363a88461daf48f48e01d70d792651720f7dff5
dsoftware56 <dsoftware56@gamil.com>
2019-06-16 21:54:19 -0500

    Se corrigieron enlaces del menu y footer, los detalles pendientes de la reunion del  depa

--- diseño2/goTop.js
+++ diseño2/goTop.js
@@ -11,8 +11,8 @@ function irArriba(){
 }
 
 // Add active class to the current button (highlight it)
-var header = document.getElementById("topnav");
-var btns = header.getElementsByClassName("menuAct");
+var header = document.getElementById("activar");
+var btns = header.getElementsByClassName("activarEnlace");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
   var current = document.getElementsByClassName("active");
