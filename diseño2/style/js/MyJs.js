7630acd105452551c85afae7b426728d462e00bd
falcarazing <falcarazing@hotmail.com>
2019-07-14 22:02:12 -0500

    se suben los sig N0 de cambios pendientes: 9,11,12,13,1,2,7,11

---
+++ diseño2/style/js/MyJs.js
@@ -0,0 +1,44 @@
+window.onload = function() {
+ /* window.scroll({
+  top: 0,
+  left: 0,
+  behavior: 'smooth'
+});
+*/
+};
+
+function scrollTo1() {
+  window.scroll({
+    behavior: 'smooth',
+    left: 0,
+    top: 560
+  });
+}
+
+function jumpToSectionServicios() {	
+	scrollTo1();
+}
+
+function scrollTo2() {
+  window.scroll({
+    behavior: 'smooth',
+    left: 0,
+    top: 1100
+  });
+}
+
+function jumpToSectionAcerca() {	
+	scrollTo2();
+}
+
+function scrollTo3() {
+  window.scroll({
+    behavior: 'smooth',
+    left: 0,
+    top: 2400
+  });
+}
+
+function jumpToSectionTecnologias() {	
+	scrollTo3();
+}
